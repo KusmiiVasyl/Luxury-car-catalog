d970415bef207a2601e9140fc2f0c0d61a09dfe5
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-04-09 16:14:32 +0300

    Create Header component, add icons slide, favorite, bag, than styling.

---
+++ src/pages/Header.jsx
@@ -0,0 +1,36 @@
+import { BsSliders } from "react-icons/bs";
+import { MdFavorite } from "react-icons/md";
+import { IoBagSharp } from "react-icons/io5";
+import styles from "./Hearder.module.css";
+import userImg from "../images/myFoto.PNG";
+
+const Header = () => {
+  return (
+    <header>
+      <a href="#" className={styles.menu}>
+        <BsSliders />
+      </a>
+      <div className={styles.userItems}>
+        <a href="#" className={styles.icon}>
+          <MdFavorite />
+          <span className={styles.favorite}>0</span>
+        </a>
+        <a href="#" className={styles.icon}>
+          <IoBagSharp />
+          <span className={styles.bag}>0</span>
+        </a>
+        <div className={styles.avatar}>
+          <a href="#">
+            <img src={userImg} alt="User Image" />
+          </a>
+          <div className={styles.user}>
+            <span>User Name</span>
+            <a href="#">View profile</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import SideMenu from "../components/SideMenu";
+import Header from "./Header";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.mainBlock}>
       <SideMenu />
+      <div className={styles.banner}>
+        <Header/>
+      </div>
     </div>
   );
 };
