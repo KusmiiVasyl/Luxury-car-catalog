b9ccb83a305190ccffb14a71c1e72059ee11f74a
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-04-19 13:08:31 +0300

    Install bootstrap, add one more block to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import CarSwiper from "../components/CarSwiper";
 import styles from "./Home.module.css";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Home = () => {
   return (
     <section id="home" className={styles.active}>
-      <div>
-        <div>
+      <div className="container-fluid">
+        <div className="row">
           <CarSwiper />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6">
+          <h2 className={styles.sectionTitle}>Rent cars on promotion</h2>
+        </div>
+        <div className="col-lg-6 d-flex justify-content-end">
+          <a href="#" className={styles.viewMore}>
+            <div className="d-flex">
+              <div>View more cars</div>
+              <div className="ms-2">
+                <FaArrowRightLong />
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </section>
