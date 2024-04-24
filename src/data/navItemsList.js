cb9e0fb8a136f2439e2fc3f9f0ddca528da69bb4
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-04-24 11:26:18 +0300

    Add path name to navItemsList.js

--- src/data/navItemsList.js
+++ src/data/navItemsList.js
@@ -1,17 +1,23 @@
 export const navItemsList = [
-  { id: 1, name: "Home", path: "#", icon: "bi bi-house", active: true },
-  { id: 2, name: "Categories", path: "#", icon: "bi bi-grid", active: false },
+  { id: 1, name: "Home", path: "home", icon: "bi bi-house", active: true },
+  {
+    id: 2,
+    name: "Categories",
+    path: "categories",
+    icon: "bi bi-grid",
+    active: false,
+  },
   {
     id: 3,
     name: "My Favorites",
-    path: "#",
+    path: "favorites",
     icon: "bi bi-heart",
     active: false,
   },
   {
     id: 4,
     name: "My Garage",
-    path: "#",
+    path: "garage",
     icon: "bi bi-taxi-front",
     active: false,
   },
