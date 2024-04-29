79986d64c05622745888dc7a1580550bfd743c8d
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-04-29 12:46:36 +0300

    Add input search and styling in Categories.jsx.

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Categories = () => {
   const carsfilterOptions = useGetData((state) => state.filterOptions);
   const [filterOption, setFilterOption] = useState(carsfilterOptions[0].value);
- 
+
   const handleChange = (e) => {
     setFilterOption(e.target.value);
   };
@@ -14,7 +14,7 @@ const Categories = () => {
     <section id="categories">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-lg-8">
+          <div className="col d-flex justify-content-start align-items-center">
             <select className={styles.filters} onChange={handleChange}>
               {carsfilterOptions.map((option) => (
                 <option key={option.id} value={option.value}>
@@ -23,7 +23,12 @@ const Categories = () => {
               ))}
             </select>
           </div>
-          <div className="col-lg-4"></div>
+          <div className="col d-flex align-items-center justify-content-lg-end justify-content-sm-start">
+            <div className={styles.search}>
+              <i className="bi bi-search"></i>
+              <input type="text" name="search" placeholder="Search" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
