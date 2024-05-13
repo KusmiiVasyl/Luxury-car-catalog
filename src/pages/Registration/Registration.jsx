80629b56cc019b48a5cd9a7bf94b262e533d008b
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-05-13 16:28:55 +0300

    Add registration form and some styling.

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,9 +1,82 @@
-import styles from './Registration.module.css'
+import { TbArrowBigRightLineFilled } from "react-icons/tb";
+import styles from "./Registration.module.css";
+import { mobileCode } from "../../data/mobileCode";
 
 const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+    <div className={styles.formContainer}>
+      <form className={styles.form}>
+        <h3>Register form</h3>
+        <div>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            placeholder="First Name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            placeholder="Last Name"
+          />
+        </div>
+        <div className="input-group">
+          <select className="form-select" id="inputMobile">
+            {mobileCode.map((item, index) => (
+              <option key={index} value={item} selected={item === "US +1"}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            className="form-control w-25"
+            placeholder="Mobile"
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            placeholder="Your age"
+            min="18"
+            max="100"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            placeholder="Driving experience"
+            min="0"
+            max="100"
+            required
+          />
+        </div>
+        <div>{/* TODO Add photo input */}</div>
+        <div className={styles.submit}>
+          <button type="submit">
+            <TbArrowBigRightLineFilled />
+            <span>Register</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Registration
+export default Registration;
