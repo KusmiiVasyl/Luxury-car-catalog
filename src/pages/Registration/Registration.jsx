dd0794ca28cddaf762bdcbd37dedfcea1d6de9bf
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-05-15 16:43:35 +0300

    Add useForm hook, and use its in Register form.

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,12 +1,25 @@
+import { useForm } from "react-hook-form";
 import { TbArrowBigRightLineFilled } from "react-icons/tb";
 import { IoMdAdd } from "react-icons/io";
 import styles from "./Registration.module.css";
 import { mobileCode } from "../../data/mobileCode";
+import { DevTool } from "@hookform/devtools";
 
 const Registration = () => {
+  const { register, control, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.formContainer}>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+      >
         <h3>Register form</h3>
         <div>
           <input
@@ -14,7 +27,11 @@ const Registration = () => {
             className="form-control"
             id="firstName"
             placeholder="First Name"
+            {...register("firstName", {
+              required: { value: true, message: "First Name is required" },
+            })}
           />
+          <p>{errors.firstName?.message}</p>
         </div>
         <div>
           <input
@@ -22,21 +39,35 @@ const Registration = () => {
             className="form-control"
             id="lastName"
             placeholder="Last Name"
+            {...register("lastName", {
+              required: { value: true, message: "Last Name is required" },
+            })}
           />
+          <p>{errors.lastName?.message}</p>
         </div>
         <div className="input-group">
-          <select className="form-select" id="inputMobile">
+          <select
+            className="form-select"
+            id="codeMobile"
+            defaultValue={"US +1"}
+            {...register("codeMobile")}
+          >
             {mobileCode.map((item, index) => (
-              <option key={index} value={item} selected={item === "US +1"}>
+              <option key={index} value={item}>
                 {item}
               </option>
             ))}
           </select>
           <input
             type="text"
+            id="phoneNumber"
             className="form-control w-25"
             placeholder="Mobile"
+            {...register("phoneNumber", {
+              required: { value: true, message: "Mobile is required" },
+            })}
           />
+          <p>{errors.phoneNumber?.message}</p>
         </div>
         <div>
           <input
@@ -44,7 +75,14 @@ const Registration = () => {
             className="form-control"
             id="email"
             placeholder="Email address"
+            {...register("email", {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/,
+                message: "Invalid email address",
+              },
+            })}
           />
+          <p>{errors.email?.message}</p>
         </div>
         <div>
           <input
@@ -55,24 +93,41 @@ const Registration = () => {
             min="18"
             max="100"
             required
+            {...register("age", {
+              required: {
+                value: true,
+                message: "Age is required",
+              },
+              min: 18,
+              max: 100,
+            })}
           />
+          <p>{errors.age?.message}</p>
         </div>
         <div>
           <input
             type="number"
             className="form-control"
-            id="age"
+            id="drivingExperience"
             placeholder="Driving experience"
             min="0"
             max="100"
-            required
+            {...register("drivingExperience", {
+              required: {
+                value: true,
+                message: "Driving experience is required",
+              },
+              min: 0,
+              max: 100,
+            })}
           />
+          <p>{errors.drivingExperience?.message}</p>
         </div>
         <div className={styles.addPhoto}>
           <p>Add photo</p>
           <label>
             <IoMdAdd />
-            <input type="file" />
+            <input type="file" id="photo" {...register("photo")} />
           </label>
         </div>
         <div className={styles.submit}>
@@ -82,6 +137,7 @@ const Registration = () => {
           </button>
         </div>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
